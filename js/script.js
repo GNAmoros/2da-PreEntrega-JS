6850ae4065aea12852415a1d6738c4c186ec29d3
Nicolas Amoros <nicolas.amoros@gmail.com>
2023-06-19 17:47:24 -0300

    services menu ready

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const services = [
     { id: 1, name: "Acido hialuronico", cost: 45000, area: "Estetica"},
-    { id: 2, name: "Dermapen", cost: 7500, category: "Estetica"},
+    { id: 2, name: "Dermapen", cost: 7500, area: "Estetica"},
     { id: 3, name: "Fosfatidilcolina", cost: 8000, area: "Estetica"},
     { id: 4, name: "Peeling quimico", cost: 4000, area: "Estetica"},
     { id: 5, name: "Pap/Colpo", cost: 1000, area: "Ginecologia"},
@@ -13,25 +13,34 @@ const services = [
     { id: 12, name: "Curso pre-parto", cost: 5000, area: "Obstetricia"}
 ]
 
-let bienvenida = "Bienvenidos a nuestra clinica de obstetricia y ginecologia\n1 -Departamentos\n2 - Sacar turno\n3 - Contacto\n0 - Salir"
+
+let bienvenida = "Bienvenidos a nuestra clinica de obstetricia y ginecologia. Como podemos ayudarle?\n1 -Estetica\n2 - Ginecologia\n3 - Obstetricia\n4 - Sacar turno\n5 - Salir"
 
 let option
+
 
 do {
     option = Number(prompt(bienvenida))
     if (option === 1) {
-        servicesList(services)
-    }
-}   while (option !==0)
+        const esteticaItems = services.filter(service => service.area === 'Estetica');
+        let esteticaOptions = esteticaItems.map((service, index) => `${index + 1}. ${service.name}`);
+        esteticaOptions = esteticaOptions.join('\n');
+        alert(`Estetica Options:\n${esteticaOptions}`);
+    } else if (option === 2) {
+        const ginecologiaItems = services.filter(service => service.area === 'Ginecologia');
+        let ginecologiaOptions = ginecologiaItems.map((service, index) => `${index + 1}. ${service.name}`);
+        ginecologiaOptions = ginecologiaOptions.join('\n');
+        alert(`Ginecologia Options:\n${ginecologiaOptions}`);
+    } else if (option === 3) {
+        const obstetriciaItems = services.filter(service => service.area === 'Obstetricia');
+        let obstetriciaOptions = obstetriciaItems.map((service, index) => `${index + 1}. ${service.name}`);
+        obstetriciaOptions = obstetriciaOptions.join('\n');
+        alert(`Obstetricia Options:\n${obstetriciaOptions}`);
+    } else if (option === 4) {
+        
+    } 
+      
+    }  
+    
+    while (option !==5)
 
-function servicesList(listArray) {
-    let areas = []
-    let list =""
-    listArray.forEach(element => {
-        if (!areas.includes(element.area)){
-            areas.push(element.area)
-            list+= element.area + "\n"
-        }
-    })
-    alert("areas disponibles:\n" + list.trim())
-}
